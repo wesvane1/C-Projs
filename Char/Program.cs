029711da880698afc5902b61e706bb816df8e0fb
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2024-03-03 11:19:02 -0700

    finished program

--- Char/Program.cs
+++ Char/Program.cs
@@ -5,11 +5,31 @@ class Program
 {
   static void Main()
   {
-    string vowels = "aeiouAEIOU";
+    string sentence = "haveaniceday";
+    string result = removeVowels(sentence);
 
-    foreach (var letter in vowels)
+    Console.WriteLine(result);
+
+    // string word = Console.Write("What is your favorite word?")
+
+    // foreach (var letter in vowels)
+    // {
+    //   Console.WriteLine(letter);
+    // }
+  }
+
+  static string removeVowels(string sentence)
+  {
+    string vowels = "aeiouAEIOU";
+    string result = "";
+
+    foreach(var letter in sentence)
     {
-      Console.WriteLine(letter);
+      if (!vowels.Contains(letter))
+      {
+        result += letter;
+      }
     }
+    return result;
   }
 }
