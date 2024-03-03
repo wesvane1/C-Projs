4e7d6b3b2810585beedc372bd9c2a86d300ed481
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2024-03-03 11:13:57 -0700

    fixed some errors and got the program to run.

--- Char/Program.cs
+++ Char/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+  static void Main()
+  {
+    string vowels = "aeiouAEIOU";
+
+    foreach (var letter in vowels)
+    {
+      Console.WriteLine(letter);
+    }
+  }
+}
